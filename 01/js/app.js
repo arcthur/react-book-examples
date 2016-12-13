8383b93f0a8069c6b70e8a11fb98a12ac18992f4
arcthur <arthurtemptation@gmail.com>
2016-12-13 11:45:16 +0800

    feat: add comment

--- 01/js/app.js
+++ 01/js/app.js
@@ -21,6 +21,8 @@ class App extends Component {
   }
 
   render() {
+    // 这里切换 select 中的选项是没有作用的，因为使用的是 defaultActiveIndex，
+    // 如果想要作用的话，要把 defaultActiveIndex 改成 activeIndex 就可以
     return (
       <div>
         <div className="operator">
