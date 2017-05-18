94732d74971cae67b8f4ccb11f9fbde140696b17
kala888 <kala.niubility@gmail.com>
2017-05-18 15:03:53 +0800

    fix detail page not redirect issue

--- 05/src/redux/configureStore.dev.js
+++ 05/src/redux/configureStore.dev.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
-import { routerReducer } from 'react-router-redux';
-import { hashHistory } from 'react-router';
+import { routerReducer, routerMiddleware } from 'react-router-redux';
+import { browserHistory } from 'react-router';
 
 import createFetchMiddleware from 'redux-composable-fetch';
 import ThunkMiddleware from 'redux-thunk';
@@ -19,7 +19,7 @@ const FetchMiddleware = createFetchMiddleware({
 });
 
 const finalCreateStore = compose(
-  applyMiddleware(ThunkMiddleware, FetchMiddleware),
+  applyMiddleware(ThunkMiddleware, FetchMiddleware, routerMiddleware(browserHistory)),
   DevTools.instrument()
 )(createStore);
 
